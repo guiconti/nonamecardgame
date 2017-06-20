a17800022ff236a6a2b58f9f6edf4528b5b03177
GuiMac <guibasconti@gmail.com>
2017-06-20 12:23:53 -0300

    Add player with cookie user

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -41,41 +41,25 @@ exports.enterGame = (req, res) => {
         eventListener.createGameChat(body.gameId.trim());
         eventEmitter.sendChatMessage(body.gameId.trim(), 'New player connected');
 
-        if (!validator.isValidCookie(req.cookies)) {
-            let userData = {
-                address: req.connection.remoteAddress
-            };
-            tokenManager.generateToken(userData).then((token) => {
-                // Set the cookie
-                res.cookie('session', token, {maxAge: 0});
-                return res.status(200).render('gameRoom', {namespace: body.gameId.trim()});
-            }, (err) => {
-                console.log(err);
-                return res.status(500).json({msg: 'Error generating cookie'});
-            });
-        } else {
-            return res.status(200).render('gameRoom', {namespace: body.gameId.trim()});
-        }
+        return res.status(200).render('gameRoom', {namespace: body.gameId.trim()});
     });
 };
 
 exports.addPlayer = (req, res) => {
 
     //  TODO: Get ID from the player
-    var body = _.pick(req.body, 'name');
     var params = _.pick(req.params, 'gameId');
-    console.log(req.body);
-    if(!validator.isValidPlayer(body)) return res.status(400).json({msg: 'Invalid player info.'});
+    if(!validator.isValidPlayer(req.userInfo)) return res.status(400).json({msg: 'Invalid player info.'});
     if(!validator.isValidGameId(params.gameId)) return res.status(400).json({msg: 'Invalid game id.'});
 
     GameModel.findById(params.gameId.trim(), (err, gameTable) => {
         if (err || !gameTable) return res.status(404).json({msg: 'Game table not found.'});
         if (gameTable.players.length >= MAX_PLAYERS) return res.status(400).json({msg: 'Game table already full.'});
-        let newPlayer = new Player(body.name.trim(), req.cookies.session);
+        let newPlayer = new Player(req.userInfo.name.trim(), req.cookies.session);
         gameTable.players.push(newPlayer);
         gameTable.save((err) => {
             if(err) return res.status(500).json({msg: 'We could not add you to the table. Try again later'});
-            eventEmitter.sendChatMessage(params.gameId.trim(), body.name.trim() + ' entered the game.')
+            eventEmitter.sendChatMessage(params.gameId.trim(), req.userInfo.name.trim() + ' entered the game.')
             return res.status(200).json({msg: 'Welcome to the table'});
         });
     });
