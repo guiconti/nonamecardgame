d0fac270429460f792973cbf299cf52c5be7bd23
GuiMac <guibasconti@gmail.com>
2017-07-23 19:21:12 -0300

    Show cards on hand on enter and start, need to update it

--- server/controllers/game/startGame.js
+++ server/controllers/game/startGame.js
@@ -5,6 +5,7 @@ const GameModel = mongoose.model('Game');
 const eventEmitter = require('../communication/eventEmitter');
 const logger = require('../../../tools/logger');
 
+const sendGameToPlayers = require('./sendGameToPlayers');
 const getTreasure = require('./treasure/getTreasure');
 const getDungeon = require('./dungeon/getDungeon');
 const treasuresList = require('./treasure/treasuresList');
@@ -46,28 +47,6 @@ module.exports = (req, res) => {
         return logger.logError(err);
     }
 };
-
-function sendGameToPlayers(gameTable) {
-
-    try{
-        gameTable.players.forEach((player) => {
-            let playerInfo = _.omit(player, '_id', 'communicationId');
-            eventEmitter.sendPrivatePlayerInfo(gameTable._id, player.communicationId, JSON.stringify(playerInfo))
-        })
-        let gameInfoCensored = {
-            players: gameTable.players.map((player) => {
-                return _.omit(player, 'communicationId', 'hand', '_id');
-            }),
-            turnInfo: gameTable.turnInfo
-        };
-        eventEmitter.sendTurnInfo(gameTable._id, JSON.stringify(gameInfoCensored.turnInfo));
-        //  Fazer isso no front com o game info?
-    } catch(err){
-        console.log(err);
-        return logger.logError(err);
-    }
-    
-}
 
 function setupGame(gameTable){
 
