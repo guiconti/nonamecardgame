2e4cf6762614e26b9835619a357a243164585556
Guilherme <guibasconti@gmail.com>
2017-08-09 18:55:01 -0300

    Discard item aways enable on front

--- server/controllers/game/dungeon/dungeonsList.js
+++ server/controllers/game/dungeon/dungeonsList.js
@@ -121,6 +121,596 @@ module.exports = function() {
             name: 'Dungeon 17',
             deck: deckType.DUNGEON,
             cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 2',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 7',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 8',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 9',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 10',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 11',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 4',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 12',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 13',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 6',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 14',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 15',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 16',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 17',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 2',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 7',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 8',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 9',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 10',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 11',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 4',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 12',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 13',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 6',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 14',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 15',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 16',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 17',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 2',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 7',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 8',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 9',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 10',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 11',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 4',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 12',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 13',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 6',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 14',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 15',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 16',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 17',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 2',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 7',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 8',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 9',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 10',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 11',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 4',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 12',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 13',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 6',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 14',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 15',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 16',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 17',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 1',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 2',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 3',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 7',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 8',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 9',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 10',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 11',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 4',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 1,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 12',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.RACE
+        },
+        {
+            name: 'Monster 5',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 3,
+            treasureReward: 1,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 13',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Monster 6',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.MONSTER,
+            combatPower: 4,
+            treasureReward: 2,
+            levelReward: 1
+        },
+        {
+            name: 'Dungeon 14',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.CURSE
+        },
+        {
+            name: 'Dungeon 15',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.ROLE
+        },
+        {
+            name: 'Dungeon 16',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
+        },
+        {
+            name: 'Dungeon 17',
+            deck: deckType.DUNGEON,
+            cardType: dungeonsType.USABLE
         }
     ]);
 };
