4a7f898697908b6104bee0edaafaa543fb6f9d5d
Guilherme <guibasconti@gmail.com>
2017-09-01 23:33:13 -0300

    Remove old dungeons and treasures from new lists

--- server/controllers/game/treasure/treasuresListNEW.js
+++ server/controllers/game/treasure/treasuresListNEW.js
@@ -9,60 +9,6 @@ const sexList = require('../../player/sexList');
 module.exports = function() {
     return([
         {
-            name: 'Potion of Idiotic Bravery',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 2
-        },
-        {
-            name: 'Potion of Halitosis',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 2
-        },
-        {
-            name: 'Nasty-Tasting Sport Drink',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 2
-        },
-        {
-            name: 'Freezing Explosive Potion',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 3
-        },
-        {
-            name: 'Eletric Radioactive Acid Potion',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 5
-        },
-        {
-            name: 'Sleep Potion',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 2
-        },
-        {
-            name: 'Potion of Confusion',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 3
-        },
-        {
-            name: 'Flaming Poison Potion',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 3
-        },
-        {
-            name: 'Magic Missile',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 5
-        },
-        {
             name: 'Cleric',
             deck: deckType.TREASURE,
             cardType: treasuresType.EQUIPMENT,
@@ -217,6 +163,60 @@ module.exports = function() {
             equipmentType: equipmentsType.RACE,
             raceType: racesList.ELF,
             sexType: sexList.FEMALE
+        },
+        {
+            name: 'Potion of Idiotic Bravery',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 2
+        },
+        {
+            name: 'Potion of Halitosis',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 2
+        },
+        {
+            name: 'Nasty-Tasting Sport Drink',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 2
+        },
+        {
+            name: 'Freezing Explosive Potion',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 3
+        },
+        {
+            name: 'Eletric Radioactive Acid Potion',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 5
+        },
+        {
+            name: 'Sleep Potion',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 2
+        },
+        {
+            name: 'Potion of Confusion',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 3
+        },
+        {
+            name: 'Flaming Poison Potion',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 3
+        },
+        {
+            name: 'Magic Missile',
+            deck: deckType.TREASURE,
+            cardType: treasuresType.CONSUMABLE,
+            bonus: 5
         },
         {
             name: 'Invoke Obscure Rules',
@@ -288,158 +288,6 @@ module.exports = function() {
             equipmentType: equipmentsType.FOOTGEAR,
             bonus: 2,
             text: '+2 Combat power'
-        },
-        // Old
-        {
-            name: 'Male Orc',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.ORC,
-            sexType: sexList.MALE
-        },
-        {
-            name: 'Female Orc',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.ORC,
-            sexType: sexList.FEMALE
-        },
-        {
-            name: 'Male Dwarf',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.DWARF,
-            sexType: sexList.MALE
-        },
-        {
-            name: 'Female Dwarf',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.Dwarf,
-            sexType: sexList.FEMALE
-        },
-        {
-            name: 'Female Halfling',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.HALFLING,
-            sexType: sexList.FEMALE
-        },
-        {
-            name: 'Male Halfling',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.HALFLING,
-            sexType: sexList.MALE
-        },
-        {
-            name: 'Female Elf',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.ELF,
-            sexType: sexList.FEMALE
-        },
-        {
-            name: 'Male Elf',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.RACE,
-            raceType: racesList.ELF,
-            sexType: sexList.MALE
-        },
-        {
-            name: 'Cleric',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.ROLE,
-            roleType: rolesList.CLERIC
-        },
-        {
-            name: 'Thief',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.ROLE,
-            roleType: rolesList.THIEF
-        },
-        {
-            name: 'Warrior',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.ROLE,
-            roleType: rolesList.WARRIOR
-        },
-        {
-            name: 'Wizard',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.ROLE,
-            roleType: rolesList.WIZARD
-        },
-        {
-            name: 'Equipment 2',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.HEAD,
-            bonus: 1
-        },
-        {
-            name: 'Treasure 3',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 2
-        },
-        {
-            name: 'Equipment 4',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.CHEST,
-            bonus: 3
-        },
-        {
-            name: 'Treasure 5',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 3
-        },
-        {
-            name: 'Equipment 6',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.ONE_HAND,
-            bonus: 2
-        },
-        {
-            name: 'Treasure 7',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 3
-        },
-        {
-            name: 'Equipment 8',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.HEAD,
-            bonus: 2
-        },
-        {
-            name: 'Treasure 9',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.CONSUMABLE,
-            bonus: 4
-        },
-        {
-            name: 'Equipment 10',
-            deck: deckType.TREASURE,
-            cardType: treasuresType.EQUIPMENT,
-            equipmentType: equipmentsType.TWO_HAND,
-            bonus: 3
         }
     ]);
 };
